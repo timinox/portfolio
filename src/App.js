35e3ba0050e2661e894ad00fad80e794de9c8906
timothejoubert <timothe.joubert@hotmail.fr>
2021-11-09 12:58:29 +0100

    change default route folder test build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //utils
 import { mapRange } from "./utils.js";
